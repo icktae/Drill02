f8615212b809ee4b2da8596b9f862eb96435fe0c
icktae <taechil77@naver.com>
2023-09-18 13:00:13 +0900

    Drill02 제출 (2019150011 김태익)
    사각이동 반복

--- character_grass.py
+++ character_grass.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 open_canvas()
 
@@ -6,37 +7,49 @@ open_canvas()
 grass = load_image('grass.png')
 character = load_image('character.png')
 
-x, y = 0, 90
-
-# 사각이동 
-while x < 770:
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x = x + 2
-    delay(0.01)
+x, y = 400, 90
 
 
-while y < 550:
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(770, y)
-    y = y + 2
-    delay(0.01)
-
-while x > 30:
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 550)
-    x = x - 2
-    delay(0.01)
-
-while y > 0:
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(30, y)
-    y = y - 2
-    delay(0.01)
+# 사각이동
+while(1):
+    while x < 770:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
+        x = x + 4
+        delay(0.01)
 
 
+    while y < 550:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(770, y)
+        y = y + 4
+        delay(0.01)
+
+    while x > 30:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, 550)
+        x = x - 4
+        delay(0.01)
+
+    while y > 90:
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(30, y)
+        y = y - 4
+        delay(0.01)
+
+    while x <400:
+        clear_canvas_now()
+        grass.draw_now(400,30)
+        character.draw_now(x,90)
+        x = x + 4
+        delay(0.01)
+        
+
+
+
+      
 close_canvas()
