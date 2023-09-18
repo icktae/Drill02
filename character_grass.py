b21cbd537c79ab8586d63e6aade7d1e24da1f302
icktae <taechil77@naver.com>
2023-09-18 13:14:24 +0900

    Drill02 제출 (2019150011 김태익)
    사각이동, 원이동 반복

--- character_grass.py
+++ character_grass.py
@@ -8,11 +8,12 @@ grass = load_image('grass.png')
 character = load_image('character.png')
 
 x, y = 400, 90
+angle = 0
 
-
-# 사각이동
+#반복
 while(1):
-    while x < 770:
+    #사각이동
+    while x < 770 :
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, 90)
@@ -20,36 +21,43 @@ while(1):
         delay(0.01)
 
 
-    while y < 550:
+    while y < 550 :
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(770, y)
         y = y + 4
         delay(0.01)
 
-    while x > 30:
+    while x > 30 :
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(x, 550)
         x = x - 4
         delay(0.01)
 
-    while y > 90:
+    while y > 90 :
         clear_canvas_now()
         grass.draw_now(400, 30)
         character.draw_now(30, y)
         y = y - 4
         delay(0.01)
 
-    while x <400:
+    while x <400 :
         clear_canvas_now()
-        grass.draw_now(400,30)
-        character.draw_now(x,90)
+        grass.draw_now(400, 30)
+        character.draw_now(x, 90)
         x = x + 4
         delay(0.01)
+
+    #원 이동
+    while angle < 360 :
+        clear_canvas_now()
+        grass.draw_now(400, 30)
+        character.draw_now(x, y)
+        x = x + math.cos(angle / 360 * 2 * math.pi) * 4
+        y = y + math.sin(angle / 360 * 2 * math.pi) * 4
+        angle = angle + 1
+        delay(0.01)
         
-
-
-
-      
+     
 close_canvas()
